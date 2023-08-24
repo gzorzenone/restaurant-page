747700a42fd41c9b1571cd7dffa683b24b37f584
gzorzenone <140647887+gzorzenone@users.noreply.github.com>
2023-08-24 13:12:04 -0300

    Add contact page

---
+++ src/contact.js
@@ -0,0 +1,45 @@
+let contacts = [
+  {
+    "name": "John Smith",
+    "role": "Owner",
+    "phoneNumber": "555-555-5555",
+    "email": "john.smith@email.com"
+  },
+  {
+    "name": "Mary Jane",
+    "role": "Manager",
+    "phoneNumber": "555-555-5556",
+    "email": "maryjane@email.com"
+  }
+]
+
+function loadContactPage() {
+  const contact = document.createElement("div");
+  contact.classList.add("contact");
+
+  contacts.forEach((value) => {
+    const person = document.createElement("div");
+    person.classList.add("person");
+    const name = document.createElement("p");
+    name.classList.add("name");
+    name.textContent = value.name;
+    person.appendChild(name);
+    const role = document.createElement("p");
+    role.classList.add("role");
+    role.textContent = value.role;
+    person.appendChild(role);
+    const phoneNumber = document.createElement("p");
+    phoneNumber.classList.add("phone-number");
+    phoneNumber.textContent = value.phoneNumber;
+    person.appendChild(phoneNumber);
+    const email = document.createElement("p");
+    email.classList.add("email");
+    email.textContent = value.email;
+    person.appendChild(email);
+    contact.appendChild(person);
+  });
+
+  return contact;
+}
+
+export default loadContactPage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import loadHomePage from "./home.js";
 import loadMenuPage from "./menu";
+import loadContactPage from "./contact";
 
 const homeTab = document.createElement("div");
 homeTab.classList.add("home-tab");
@@ -10,11 +11,16 @@ const menuTab = document.createElement("div");
 menuTab.classList.add("menu-tab");
 menuTab.textContent = "Menu";
 
+const contactTab = document.createElement("div");
+contactTab.classList.add("contact-tab");
+contactTab.textContent = "Contact";
+
 const content = document.createElement("div");
 content.classList.add("content");
 
 document.body.appendChild(homeTab);
 document.body.appendChild(menuTab);
+document.body.appendChild(contactTab);
 
 document.body.appendChild(content);
 
@@ -26,4 +32,9 @@ homeTab.addEventListener("click", () => {
 menuTab.addEventListener("click", () => {
   content.replaceChildren();
   content.appendChild(loadMenuPage());
+});
+
+contactTab.addEventListener("click", () => {
+  content.replaceChildren();
+  content.appendChild(loadContactPage());
 });
